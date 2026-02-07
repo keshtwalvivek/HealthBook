dedfd57d5c72432a24f32e2efe200d44cfaafbed
vivekkeshtwal09@gmail.com <116489124+keshtwalvivek@users.noreply.github.com>
2026-02-08 00:59:32 +0530

    changes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -7,8 +7,8 @@ import {
   bookAppointment,
   listAppointment,
   cancelAppointment,
-  //   paymentRazorpay,
-  //   verifyRazorpay,
+  // paymentRazorpay,
+  // verifyRazorpay,
 } from "../controllers/userController.js";
 import authUser from "../middlewares/authUser.js";
 import upload from "../middlewares/multer.js";
@@ -22,12 +22,12 @@ userRouter.post(
   "/update-profile",
   upload.single("image"),
   authUser,
-  updateProfile
+  updateProfile,
 );
 userRouter.post("/book-appointment", authUser, bookAppointment);
 userRouter.get("/appointments", authUser, listAppointment);
 userRouter.post("/cancel-appointment", authUser, cancelAppointment);
-// userRouter.post("/payment-razorpay", authUser, paymentRazorpay)
-// userRouter.post("/verifyRazorpay", authUser, verifyRazorpay)
+// userRouter.post("/payment-razorpay", authUser, paymentRazorpay);
+// userRouter.post("/verifyRazorpay", authUser, verifyRazorpay);
 
 export default userRouter;
